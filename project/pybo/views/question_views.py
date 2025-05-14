735a989791bb6e4feb04cdb12c491eee256dbd5e
gjwjddbs <apexgenuine@gmail.com>
2025-05-14 11:51:09 +0900

    feat: 질문 등록 기능 구현 및 폼 클래스 추가
    - question_list.html에 질문 등록 버튼 추가
    - forms.py 생성하여 질문, 답변 등록 폼 클래스 정의
    - question_form.html 생성하여 질문 등록 페이지 작성
    - answer_views.py에서 답변 등록 라우터 함수 수정
    - question_views.py에서 질문 등록 및 질문 상세 페이지 라우터 함수 수정

--- project/pybo/views/question_views.py
+++ project/pybo/views/question_views.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
-
+from datetime import datetime
+from flask import Blueprint, render_template, request,url_for
+from ..forms import QuestionForm, AnswerForm
 from pybo.models import Question, Answer
+from werkzeug.utils import redirect
+from .. import db
 
 bp = Blueprint('question',__name__,url_prefix='/question')
 
@@ -13,5 +16,18 @@ def _list():
 #질문 상세 페이지
 @bp.route('/datail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html',question=question)
+    return render_template('question/question_detail.html',question=question,form=form)
+
+#질문 등록 페이지
+@bp.route('/create/', methods=['GET','POSt'])
+def create():
+    form = QuestionForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        question = Question(subject=form.subject.data,content=form.content.data,create_date=datetime.now())
+        db.session.add(question)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('question/question_form.html',form=form)
+
