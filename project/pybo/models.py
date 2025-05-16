a2817b00ab3bd5a0fd2602b4f61db412172db37a
gjwjddbs <apexgenuine@gmail.com>
2025-05-16 11:39:19 +0900

    feat: 회원가입 기능 구현
    - models.py에 사용자 모델(User) 생성
    - forms.py에 회원가입 폼 클래스 추가
    - auth_views.py 생성하여 회원가입 라우터 구현
    - __init__.py에 회원가입 블루프린트 등록
    - signup.html 생성하여 회원가입 템플릿 작성
    - form_errors.html 생성하여 입력 오류 메시지 출력 처리
    - navbar.html에 회원가입 링크 추가

--- project/pybo/models.py
+++ project/pybo/models.py
@@ -20,3 +20,14 @@ class Answer(db.Model):
     content = db.Column(db.Text(), nullable=False)
     #답변 작성일시
     create_date = db.Column(db.DateTime(), nullable=False)    
+
+class User(db.Model):
+    #유저 고유 번호
+    id =db.Column(db.Integer,primary_key=True)
+    #유저 ID
+    username=db.Column(db.String(150),unique=True,nullable=False)
+    #유저 비밀번호
+    password=db.Column(db.String(200),nullable=False)
+    #유저 이메일
+    email=db.Column(db.String(120),unique=True,nullable=False)
+    
